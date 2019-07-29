d14f48e24e737878106a857aa8b67cb038456912
Andrew Jakubowicz <spyr1014@gmail.com>
2019-07-28 22:40:00 -0700

    checkpoint moves

--- Scripts/PlayerSpawner.gd
+++ Scripts/PlayerSpawner.gd
@@ -3,6 +3,8 @@ extends Position2D
 onready var player = load("res://Prefabs/Player.tscn")
 onready var camera = load("res://Prefabs/CustomCamera2D.tscn")
 
+var curr_player = null
+
 func spawn_player ():
 	var p = player.instance()
 	p.position = position
@@ -10,4 +12,9 @@ func spawn_player ():
 	p.friction = 20
 	get_tree().get_root().add_child(p)
 	p.add_child(camera.instance())
+	curr_player = p
 
+
+
+func _on_ScoreBall_collected_checkpoint(global_position):
+	position = global_position
