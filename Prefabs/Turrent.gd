dae5bb75cdeab10d304da2e5103b115a2c29fc68
Andrew Jakubowicz <spyr1014@gmail.com>
2019-08-04 18:12:11 -0700

    finished level 5

--- Prefabs/Turrent.gd
+++ Prefabs/Turrent.gd
@@ -1,30 +1,49 @@
 extends Node2D
 
-onready var target = $Target
 onready var gun_top = $gun_top
 var player_to_follow = null
-
 var _flipped_state = false
 
 onready var Bullet = load("res://Prefabs/Bullet.tscn") as PackedScene
+onready var _rotated_state = 360 > abs(fmod(rotation_degrees, 360)) and abs(fmod(rotation_degrees, 360)) > 178
+onready var fire_rate_timer = $Timer
+onready var tween = $Tween
 
+enum FireMode {
+	STRAIGHT
+	GUESS_SHOT
+}
 
+export (float) var fire_rate_sec = 1.5
+export (float) var random_scatter_fire_rate = 0
+export (float) var initial_delay = 0
+export (FireMode) var mode = FireMode.STRAIGHT
 
+export (NodePath) var override_target_path
+var override_target
+
+func _ready():
+	if override_target_path:
+		override_target = get_node(override_target_path)
+	fire_rate_timer.wait_time = fire_rate_sec
+	start_shot_timer(initial_delay)
+
+func start_shot_timer(delay=0):
+	fire_rate_timer.start(fire_rate_sec + rand_range(0, random_scatter_fire_rate) + delay)
 
 func _physics_process(delta):
-	if player_to_follow:
-		gun_top.look_at(player_to_follow.position)
+	if override_target:
+		tween.interpolate_property(gun_top, "rotation", gun_top.rotation, override_target.position.angle_to_point(gun_top.position), 0.2, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	elif player_to_follow:
+		tween.interpolate_property(gun_top, "rotation", gun_top.rotation, gun_top.global_position.angle_to_point(player_to_follow.global_position), 0.4, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	else:
-		gun_top.look_at(target.position)
+		return
+	tween.start()
 	var rot_normal = fmod(gun_top.rotation_degrees, 360)
 	if rot_normal < 270 and rot_normal > 90:
 		_switch(true)
 	else:
 		_switch(false)
-
-
-
-
 
 
 func _switch(to_new_state):
@@ -37,11 +56,6 @@ func _switch(to_new_state):
 	else:
 		gun_top.scale *= -1
 
-
-
-
-
-
 ## GROUP METHOD CALLS
 
 func set_current_player(p):
@@ -52,10 +66,11 @@ func unset_current_player():
 
 
 func _on_Timer_timeout():
-	print('bullllet fiiiiired')
 	var b = Bullet.instance()
 	var direction = Vector2(cos(gun_top.rotation), sin(gun_top.rotation))
-	b.position = position + Vector2(0, -10) + (direction.normalized() * 5)
+	var gun_end_offset = -5 if _rotated_state else 5
+	b.position = position + (Vector2(cos(global_rotation-PI/2), sin(global_rotation-PI/2)).normalized() * 10) + (direction.normalized() * gun_end_offset)
 	# TODO figure out bullet velocities
-	b.applied_force = direction.normalized() * 10
 	get_parent().add_child(b)
+	b.apply_impulse(Vector2(), direction.normalized() * 200 * (-1 if _rotated_state else 1))
+	start_shot_timer()
