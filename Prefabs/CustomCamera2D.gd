04b5416e2a679359d38d1c93ef4d0aa074cb142e
Andrew Jakubowicz <spyr1014@gmail.com>
2019-08-09 08:32:38 -0700

    fixed camera system

--- Prefabs/CustomCamera2D.gd
+++ Prefabs/CustomCamera2D.gd
@@ -9,20 +9,24 @@ func _ready():
 	tween_limits.connect("tween_all_completed", self, "_on_limitsTween_tween_all_completed")
 
 func camera(global_pos: Vector2, extents: Vector2):
+	# Ensures that if camera is already set it doesn't freeze player.
+	if global_pos.x - extents.x - 8 == limit_left and camera_moves != 0:
+		return
+
 	camera_moves += 1
 	var duration = 0.2
 	if camera_moves == 1:
-		limit_left = global_pos.x - extents.x
-		limit_right = global_pos.x + extents.x
-		limit_top = global_pos.y - extents.y
-		limit_bottom = global_pos.y + extents.y
+		limit_left = global_pos.x - extents.x - 8
+		limit_right = global_pos.x + extents.x + 8
+		limit_top = global_pos.y - extents.y - 8
+		limit_bottom = global_pos.y + extents.y + 8
 		return
 		
 	get_tree().call_group("freeze", "freeze")
-	var limit_left_end = global_pos.x - extents.x
-	var limit_right_end = global_pos.x + extents.x
-	var limit_top_end = global_pos.y - extents.y
-	var limit_bottom_end = global_pos.y + extents.y
+	var limit_left_end = global_pos.x - extents.x - 8
+	var limit_right_end = global_pos.x + extents.x + 8
+	var limit_top_end = global_pos.y - extents.y - 8
+	var limit_bottom_end = global_pos.y + extents.y + 8
 
 	tween_limits.interpolate_property(self, "limit_left", limit_left, limit_left_end, duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	tween_limits.interpolate_property(self, "limit_right", limit_right, limit_right_end, duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
